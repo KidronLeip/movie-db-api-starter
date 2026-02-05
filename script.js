cc5feed6ee1a11833c1e52d95eabddacdeadfeb0
kleip <kleip@asu.edu>
2026-02-05 15:45:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // endpoint here: https://developer.themoviedb.org/reference/movie-popular-list
 function getPopularMovies(){
     // the endpoint
-    // TO DO
+    let url = "https://api.themoviedb.org/3/movie/popular?api_key=501f5b76a796fbb87924ba46d92b598f&language=en-US";
     // the place on the page where we'll display the movies
     let popularMovies = document.getElementById("popular");
     let imgUrl = "https://image.tmdb.org/t/p/w400";
@@ -10,22 +10,28 @@ function getPopularMovies(){
 
     // ajax time!
     // create the object
-    // TO DO
+    let xhr = new XMLHttpRequest();
 
     // attach event handlers
-    // TO DO
-    /*
-        // This code can be used for the display of the featured movie
-        // (it is a string template)
-            `<section id="featured">
-                <h3>${"TO DO"}</h3>
-                <img src="${"TO DO"}" alt="">
-                <p>"${"TO DO"}"</p>
-            </section>`
+    xhr.addEventListener("readystatechange", function() {
+        // is the request done?
+        if(this.readyState === this.DONE) {
+            let json = this.response;
+
+            // string for output
+            let html = "";
+
+            // This code can be used for the display of the featured movie
+            // (it is a string template)
+            html += `<section id="featured">
+                <h3>${json.results[0].title}</h3>
+                <img src="${imgUrl}${json.results[0].poster_path}" alt="">
+                <p>"${json.results[0].overview}"</p>
+            </section>`;
 
 
-        // This code can be used for the display of the other popular movies (18 of them)
-        // (it is a string template)
+            // This code can be used for the display of the other popular movies (18 of them)
+            // (it is a string template)
             `<section class="movie">
                 <img src="${"TO DO"}" alt="">
                 <div>
@@ -35,15 +41,19 @@ function getPopularMovies(){
                     </p>
                 </div>
             </section>`
-        
-    */
+
+            popularMovies.innerHTML = html;
+        }  
+    });
+
     // set the response type
-    // TO DO
+    xhr.responseType = "json";
+    
     // open the request
-    // TO DO
+    xhr.open("GET", url);
 
     // send the request
-    // TO DO
+    xhr.send();
 }
 
 // function runs only after a year is entered/chosen and submitted through the form
